8290c7ec6a690f228bdd5aa1cd3ffaee16a4d34c
robin80577 <robinbiagioni97@gmail.com>
2022-04-12 14:41:56 +0200

    update chomage

--- index.js
+++ index.js
@@ -2,6 +2,12 @@
 
 // const express= require('express');
 
+// import https from "https";
+// import fs from "fs";
+// import path from "path";
+// import xlsx from "node-xlsx";
+
+import XLSX from "xlsx";
 import fetch from "node-fetch";
 import express from 'express';
 import axios from 'axios';
@@ -9,9 +15,11 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 
+
 app.get('/', function(request, response){
 	response.send('bienvenue sur mon serveur');
 })
+
 
 app.get('/aide_territoire', function(request, response){
 	response.send('hello')
@@ -37,7 +45,7 @@ app.get('/aide_territoire', function(request, response){
 						    data[element['fields']['reg_name']]['reg_code'] = element['fields']['reg_code'];
 						  }
 						})
-				    	
+
 	    	// console.log(element['fields']['reg_name']));
 	    console.log(data)
 	  })
@@ -50,6 +58,47 @@ app.get('/aide_territoire', function(request, response){
 
 	// console.log(api_response);
 })
+
+
+
+app.get('/chomage', function(request, response){ // NE FONCTIONNE QU'EN LOCAL
+	response.send("hello chomage");
+
+
+	const file = XLSX.readFile('./data/chomage.xlsx')
+	//const file = XLSX.readFile('https://www.insee.fr/fr/statistiques/fichier/2012804/sl_etc_2021T4.xls') // NE FONCTIONNE PAS ---- comment récupérer le fichirer dirécement via url ? ----
+
+	const sheets = file.SheetNames //stocker le nom des feuilles
+	console.log('Feuilles du fichier Excel source : ' + sheets); // afficher le nom des feuilles
+
+	// const thirdSheet = file.Sheets[sheets[2]];
+	// const ff23 = thirdSheet['FF23']; // lire une cellule spécifique
+	// console.log(Object.values(ff23)[2] + ' %')
+
+	const chom_regionsSheet = file.Sheets[sheets[2]];
+	console.log("");
+	console.log('Chômage par région (T4 2021) : ' + sheets);
+	console.log("");
+
+	for(let i=5;i<24;i++) {
+	 const regionColumn = chom_regionsSheet['B' + i];
+	 const t4_2021Column = chom_regionsSheet['FF' + i];
+	 //console.log(Object.values(t4_2021Column));
+	 console.log(Object.values(regionColumn)[2] + ' : ' + Object.values(t4_2021Column)[2] + ' %');
+
+	}
+
+
+
+	// https://www.insee.fr/fr/statistiques/fichier/2012804/sl_etc_2021T4.xls
+
+})
+
+
+
+
+
+
 
 app.listen(PORT, function(){
 	console.log('Hello :'+ PORT);
