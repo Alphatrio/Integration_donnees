9a60b3a172b8837ebbc17eb0a690afcdad73135a
zaineb KHARFATI <zaineb_kharfati@outlook.fr>
2022-04-14 19:34:26 +0200

    commit

--- index.js
+++ index.js
@@ -39,108 +39,6 @@ app.get('/', function(req, response){
 	response.send('bienvenue sur mon serveur');
 })
 
-<<<<<<< HEAD
-
-app.get('/classementslycees', function(req, response){
-	(async () => {
-        var allLycee = [];
-        for(let pagenb = 1;pagenb <= 3; pagenb++){
-            console.log('Hello')
-            const browser = await puppeteer.launch({
-				args: [
-				  '--no-sandbox',
-				  '--disable-setuid-sandbox',
-				],
-				headless:true
-			  });
-            const page = await browser.newPage();
-            if(pagenb == 1){
-                await page.goto(`https://www.letudiant.fr/palmares/classement-lycees/`);
-            }
-            else{
-                await page.goto(`https://www.letudiant.fr/palmares/classement-lycees/page-${pagenb}`);
-            }
-            const lycee = await page.evaluate(() => {
-                let lycee = [];
-                let elements = document.querySelectorAll('.c-table--housemd > tbody:nth-child(2) > tr');
-                for (ligne of elements) {
-
-                    lycee.push({
-                        lycee: ligne.querySelector('td > a').text,
-                        note: ligne.querySelector('td:nth-child(1)')?.textContent,
-                        2022: ligne.querySelector('td:nth-child(2)')?.textContent,
-                        2021: ligne.querySelector('td:nth-child(3)')?.textContent,
-                        dpt: ligne.querySelector('td:nth-child(5)')?.textContent,
-                        ville: ligne.querySelector('td:nth-child(6)')?.textContent,
-                        statut: ligne.querySelector('td:nth-child(7)')?.textContent,
-                        presbac:  ligne.querySelector('td:nth-child(8)')?.textContent,
-                        resbac: ligne.querySelector('td:nth-child(9)')?.textContent,
-                        mensbac: ligne.querySelector('td:nth-child(10)')?.textContent
-                    })
-                }
-                return lycee;
-            });
-            allLycee = [...allLycee, ...lycee];
-            //await browser.close();
-            console.log("page " + pagenb);
-        }
-        response.send(allLycee);
-    })();
-
-})
-
-
-app.get('/aide_territoire', function(req, response){
-	const data = {}
-	axios
-	  .get('https://public.opendatasoft.com/api/records/1.0/search/?dataset=demographyref-france-pop-legale-region-millesime&rows=80')
-	  .then(res => {
-	    console.log(`statusCode: ${res.status}`)
-	    // console.log(res)
-	    // console.log(res['data']['records'])
-
-
-	    res['data']['records'].forEach(element =>{
-	    				if (element['fields']['reg_name'] in data) {
-						    if('date' in data[element['fields']['reg_name']]){
-						    	if(element['fields']['start_year'] > data[element['fields']['reg_name']]['date']){
-						    		data[element['fields']['reg_name']]['date'] = element['fields']['start_year'];
-						    		data[element['fields']['reg_name']]['pop_total'] = element['fields']['reg_pop_tot'];
-						    	}
-						    }
-						  }else{
-						  	data[element['fields']['reg_name']] = {}
-						  	data[element['fields']['reg_name']]['date'] = element['fields']['start_year'];
-						    data[element['fields']['reg_name']]['pop_total'] = element['fields']['reg_pop_tot'];
-						    data[element['fields']['reg_name']]['reg_code'] = element['fields']['reg_code'];
-						  }
-						})
-
-	    	// console.log(element['fields']['reg_name']));
-	    console.log(data)
-	    response.send(data);
-	  })
-	  .catch(error => {
-	    console.error(error)
-	  })
-
-	// const api_response = fetch('https://aides-territoires.beta.gouv.fr/api/aids/');
-	// // const api_data = await api_response.json();
-
-	// console.log(api_response);
-})
-
-
-
-app.get('/chomage', function(req, response){
-
-	request('https://www.insee.fr/fr/statistiques/fichier/2012804/sl_etc_2021T4.xls', {encoding: null}, function(err, res, data) {
-	    if(err || res.statusCode !== 200) return;
-	    fs.writeFileSync('./data/chomage.xls', data);
-	});
-
-	const file = XLSX.readFile('./data/chomage.xls')
-=======
 app.get('/dep_reg', function(req, response){
 	console.log('Guten tag');
 
@@ -176,7 +74,6 @@ app.get('/dep_reg', function(req, response){
 
 	});
 
->>>>>>> f552ecb7780c584f21953ede0a2a54a24783bed7
 
 
 
@@ -184,16 +81,11 @@ app.get('/classementslycees', function(req, response){
 
 	response.send(scrap());
 
-<<<<<<< HEAD
-	console.log(data);
-	response.send(data);
-=======
 })
 
 
 app.get('/aide_territoire', function(req, response){
 	response.send(aide());
->>>>>>> f552ecb7780c584f21953ede0a2a54a24783bed7
 })
 
 
